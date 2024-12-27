c46a83464104883960358eef5f0ed5d1667f7c6f
simpsonman <lng515@naver.com>
2024-12-27 13:21:39 +0900

    회원가입규칙추가

--- src/auth/views/RegisterView.vue
+++ src/auth/views/RegisterView.vue
@@ -6,7 +6,7 @@
       </q-card-section>
 
       <q-card-section>
-        <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-form @submit="onSubmit" class="q-gutter-lg">
           <q-input
             v-model="email"
             :label="$t('auth.email')"
@@ -15,12 +15,41 @@
             :rules="[(val) => !!val || $t('login.required')]"
           />
 
+          <div class="row q-col-gutter-md">
+            <div class="col-8">
+              <q-input
+                v-model="firstName"
+                :label="$t('auth.firstName')"
+                outlined
+                :rules="[(val) => !!val || $t('login.required')]"
+              />
+            </div>
+            <div class="col-4">
+              <q-input
+                v-model="lastName"
+                :label="$t('auth.lastName')"
+                outlined
+                :rules="[(val) => !!val || $t('login.required')]"
+              />
+            </div>
+          </div>
+
+          <q-input
+            v-model="nickname"
+            :label="$t('auth.nicknameOptional')"
+            outlined
+          />
+
           <q-input
             v-model="password"
             :label="$t('auth.password')"
             type="password"
             outlined
-            :rules="[(val) => !!val || $t('login.required')]"
+            :rules="[
+              (val) => !!val || $t('login.required'),
+              (val) => validatePassword(val) || $t('auth.passwordRule'),
+            ]"
+            :hint="$t('auth.passwordHint')"
           />
 
           <q-input
@@ -39,6 +68,7 @@
               type="submit"
               color="primary"
               :label="$t('common.register')"
+              class="full-width"
             />
           </div>
         </q-form>
@@ -59,6 +89,9 @@ import { ref } from "vue";
 const email = ref("");
 const password = ref("");
 const confirmPassword = ref("");
+const firstName = ref("");
+const lastName = ref("");
+const nickname = ref("");
 
 const onSubmit = () => {
   // TODO: 회원가입 로직 구현
@@ -66,7 +99,22 @@ const onSubmit = () => {
     email: email.value,
     password: password.value,
     confirmPassword: confirmPassword.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    nickname: nickname.value,
   });
+};
+
+const validatePassword = (password: string): boolean => {
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const isLongEnough = password.length >= 8;
+
+  return (
+    hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar && isLongEnough
+  );
 };
 </script>
 
